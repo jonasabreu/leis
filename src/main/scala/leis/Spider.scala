f42303080b6c23e1f9c910d4b9f57a12a15b66e9
Jonas Abreu <jonas@vidageek.net>
2012-07-02 00:51:04 -0300

    spider funcionando

--- src/main/scala/leis/Spider.scala
+++ src/main/scala/leis/Spider.scala
@@ -1,15 +1,21 @@
 package leis
 
 import java.io.File
-
 import net.vidageek.crawler.config.CrawlerConfigurationBuilder
-import net.vidageek.crawler.{Url, Status, PageVisitor, PageCrawler, Page}
+import net.vidageek.crawler.{ Url, Status, PageVisitor, PageCrawler, Page }
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.io.PrintWriter
+import net.vidageek.crawler.component.Downloader
+import net.vidageek.crawler.component.WebDownloader
+import net.vidageek.crawler.component.LinkNormalizer
+import net.vidageek.crawler.component.DefaultLinkNormalizer
 
 object Spider {
 
   def main(args : Array[String]) {
     val cfg = new CrawlerConfigurationBuilder("http://www4.planalto.gov.br/legislacao").
-      withMaxParallelRequests(20).withRequestDelay(1000).build
+      withMaxParallelRequests(20).withRequestDelay(500).withLinkNormalizer(Normalizer()).build
     val crawler = new PageCrawler(cfg)
     crawler.crawl(Visitor())
   }
@@ -18,12 +24,26 @@ object Spider {
 
 case class Visitor() extends PageVisitor {
 
-  val directory = new File(System.getProperty("user.home"))
+  val directory = new File(System.getProperty("user.home"), "leis/" + new SimpleDateFormat("yyyy-MM-dd").format(new Date))
+  directory.mkdirs
+
+  implicit def addAsFileName(page : Page) = new {
+    def asFileName = {
+      var url = page.getUrl
+      if (url.contains("#")) url = url.substring(0, url.indexOf("#"))
+      url.replaceAll("[^A-Za-z0-9\\.\\-]", "_")
+    }
+  }
 
   def visit(page : Page) : Unit = {
     println("Visitando " + page.getUrl)
-    if (page.getUrl.toLowerCase.contains("ccivil_3")) {
-
+    if (page.getUrl.toLowerCase.contains("ccivil_03")) {
+      val writer = new PrintWriter(new File(directory, page.asFileName))
+      try {
+        writer.print(page.getContent)
+      } finally {
+        writer.close
+      }
     }
   }
 
@@ -38,6 +58,24 @@ case class Visitor() extends PageVisitor {
   private def isDomainAllowed(url : Url) : Boolean = {
     List("http://www4.planalto.gov.br", "http://www.planalto.gov.br").
       foldLeft(false)((acc, domain) => url.link.startsWith(domain) || acc)
+  }
+
+  private def isLawPage(url : Url) : Boolean = {
+    url.link.toLowerCase.contains("ccivil_03")
+  }
+
+}
+
+case class Normalizer extends LinkNormalizer {
+
+  val normalizer = new DefaultLinkNormalizer("http://www4.planalto.gov.br/legislacao")
+
+  def normalize(url : String) : String = {
+    val toReturn = normalizer.normalize(url)
+    if (toReturn.contains("#"))
+      toReturn.substring(0, toReturn.indexOf("#"))
+    else
+      toReturn
   }
 
 }
